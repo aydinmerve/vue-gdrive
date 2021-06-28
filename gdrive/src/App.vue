11f34dfd479c8e79c3ce9711aeacddf923cdcd5e
aydinmerve <merve9328@gmail.com>
2021-06-28 17:24:16 +0300

    google drive integrations added

--- gdrive/src/App.vue
+++ gdrive/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img width="auto" src="./assets/Vue-logo.png" /> <GDriveSelector />
   </div>
 </template>
 
 <script>
+import GDriveSelector from "./components/GDriveSelector";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    GDriveSelector
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
